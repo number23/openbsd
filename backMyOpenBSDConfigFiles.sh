28f623922e3b46f542a02e33535c51aa612739bc
number23 <none@none>
2010-09-29 23:42:11 +0000

    add backup script file

---
+++ backMyOpenBSDConfigFiles.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd $HOME/hgroot/bitbucket.org/openbsd
+
+cp -rf $HOME/.zprofile zprofile
+cp -rf $HOME/.aliasrc.d/S10_general aliasrc.S10_general
+
+cp -rf $HOME/.xinitrc xinitrc
+cp -rf $HOME/.xserverrc xserverrc
+cp -rf $HOME/.Xdefaults Xdefaults
